e9da8562dd57d0ffe0613688b6a12bef33abd9b5
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2016-10-07 12:08:53 +0200

    Initial commit - code as is from the lab

---
+++ viz-labs/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace viz_labs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(1, -1);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(278, 126);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 131);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 256);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ viz-labs/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace viz_labs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            simpleOpenGlControl1.InitializeContexts();
+
+            Glu.gluOrtho2D(0, 500, 0, 500); // description https://www.opengl.org/sdk/docs/man2/xhtml/gluOrtho2D.xml
+
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Width, this.simpleOpenGlControl1.Height);// http://learnopengl.com/#!Getting-started/Coordinate-Systems
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            Gl.glClearColor(0f, 0f, 0f, 0);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            ////////////////////////////////////
+
+            Gl.glColor3d(1f, 0f, 0f);
+            Gl.glBegin(Gl.GL_LINES);
+
+            Gl.glVertex2d(10, 10);
+            Gl.glVertex2d(50, 100);
+
+            Gl.glColor3d(1f, 1f, 0f);
+
+            Gl.glVertex2d(50, 100);
+            Gl.glVertex2d(100, 300);
+
+            Gl.glEnd();
+            ////////////////////////////////////
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.FillRectangle(Brushes.White, 0, 0, panel1.Width, panel1.Height);
+            return;
+            g = panel1.CreateGraphics();
+            //g.DrawLine(Pens.Black, new Point(0, 0), new Point(30, 30));
+            ///////////////////////////////////////////////////////////////
+            g.FillRectangle(Brushes.Yellow, 30.0f, 30.0f, 500.0f, 500.0f);
+            ///////////////////////////////////////////////////////////////
+            Brush b = new LinearGradientBrush(
+                new Rectangle(0, 0, 500, 500),
+                Color.Red,
+                Color.Blue,
+                LinearGradientMode.Horizontal);
+
+            g.FillRectangle(b, 0.0f, 0.0f, 500.0f, 500.0f);
+            // /////////////////////////////////////////////////////////////////
+        }
+    }
+}
